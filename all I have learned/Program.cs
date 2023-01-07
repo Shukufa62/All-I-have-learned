597c39b147bfdd66fd9d107eda3ce2b092438c3e
Shukufa62 <119412605+Shukufa62@users.noreply.github.com>
2023-01-07 19:29:59 +0400

    Add files via upload

---
+++ all I have learned/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace all_I_have_learned
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
